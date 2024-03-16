ea7f39669ca29fff896553da8ed7ba07fe684306
Milad Joodi <winbeta.com@gmail.com>
2024-03-17 03:11:21 +0330

    up

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -22,19 +22,21 @@ const data = [
 ];
 
 function Slider() {
-    const [currentSlide, setCurrentSlide] = useState(0)
+    const [currentSlide, setCurrentSlide] = useState(1)
 
     return (
         <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
             {/* Text Container */}
             <div className='h-1/2 flex flex-col items-center justify-center gap-8 text-red-500 font-bold'>
-                <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl'>Test</h1>
+                <h1 className='text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl'>
+                    {data[currentSlide].title}
+                </h1>
                 <button className='bg-red-500 text-white py-4 px-8'>Order Now</button>
             </div>
 
             {/* Image Container */}
             <div className='w-full h-1/2 relative'>
-                <Image src="/slide1.png" alt='' fill className='object-cover' />
+                <Image src={data[currentSlide].image} alt='' fill className='object-cover' />
             </div>
         </div>
     )
