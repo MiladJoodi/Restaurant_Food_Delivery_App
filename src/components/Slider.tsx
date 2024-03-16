8ebb32e21268353d1338cda16afacaa542a0ea91
Milad Joodi <winbeta.com@gmail.com>
2024-03-16 22:53:41 +0330

    Update Slider.tsx

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Slider() {
-  return (
-    <div>Slider</div>
-  )
+    return (
+        <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]'>
+            {/* Text Container */}
+            <div className='h-1/2'>
+                <h1 className=''>Test</h1>
+                <button className=''>Order</button>
+            </div>
+
+            {/* Image Container */}
+            <div className='w-full h-1/2 relative'>
+                <Image src="/slide1.png" alt='' fill className='object-cover'/>
+            </div>
+        </div>
+    )
 }
 
 export default Slider
