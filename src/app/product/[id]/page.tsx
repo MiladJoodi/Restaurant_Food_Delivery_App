cce3d4a6e493eb018f0ef6b778f587a16bdfae13
Milad Joodi <winbeta.com@gmail.com>
2024-03-18 22:32:05 +0330

    0

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -8,13 +8,13 @@ function SingleProduct() {
     <div className='p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row'>
       {/* Image Container */}
       {singleProduct.img && (
-        <div className='relative'>
+        <div className='relative w-full h-1/2'>
           <Image src={singleProduct.img} alt="" className="object-contain" fill />
         </div>
       )}
       {/* Text Container */}
-      <div className=''>
-        <h1>{singleProduct.title}</h1>
+      <div className='h-1/2 flex flex-col gap-4'>
+        <h1 className='text-3xl font-bold uppercase'>{singleProduct.title}</h1>
         <p>{singleProduct.desc}</p>
         <Price price={singleProduct.price} id={singleProduct.id} options={singleProduct.options} />
       </div>
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 type Props = {
     price: number;
@@ -7,28 +8,43 @@ type Props = {
 }
 
 function Price({ price, id, options }: Props) {
+
+    const [total, setTotal] = useState(price)
+    const [quantity, setQuantity] = useState(1)
+    const [selected, setSelected] = useState(0)
+
     return (
-        <div>
-            <h2>${price.toFixed(2)}</h2>
+        <div className='flex flex-col gap-4'>
+            <h2 className='text-2xl font-bold'>${total.toFixed(2)}</h2>
             {/* Options Container */}
-            <div className=''>
-                {options?.map(option => (
-                    <button key={option.title}>{option.title}</button>
+            <div className='flex gap-4'>
+                {options?.map((option, index) => (
+                    <button
+                        key={option.title}
+                        className='min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md'
+                        style={{
+                            background: selected === index ? "rgb(248 113 113)" : "white",
+                            color: selected === index ? "white" : "red"
+                        }}
+                        onClick={()=> setSelected(index)}
+                        >
+                        {option.title}
+                    </button>
                 ))}
             </div>
             {/* Quantity and btn container */}
-            <div className=''>
+            <div className='flex justify-between items-center'>
                 {/* Quantity */}
-                <div className=''>
+                <div className='flex justify-between w-full p-3 ring-1 ring-red-500'>
                     <span>Quantity</span>
-                    <div className=''>
-                        <button>{'<'}</button>
-                        <span>1</span>
-                        <button>{'>'}</button>
+                    <div className='flex items-center gap-4'>
+                        <button onClick={()=>setQuantity((prev)=> (prev > 1 ? prev - 1 : 1))}>{'<'}</button>
+                        <span>{quantity}</span>
+                        <button onClick={()=>setQuantity((prev)=> (prev < 9 ? prev + 1 : 9))}>{'>'}</button>
                     </div>
                 </div>
                 {/* Cart Button */}
-                <button>Add to Cart</button>
+                <button className='uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-500'>Add to Cart</button>
             </div>
         </div>
     )
