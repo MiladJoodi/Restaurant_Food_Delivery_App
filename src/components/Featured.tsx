c1a16617473274e27796fd8223fc2559e76eac32
Milad Joodi <winbeta.com@gmail.com>
2024-03-18 18:35:19 +0330

    up

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,28 +1,38 @@
-import Image from 'next/image'
-import React from 'react'
+import { featuredProducts } from "@/data";
+import Image from "next/image";
+import React from "react";
 
-function Featured() {
+const Featured = () => {
   return (
-    <div className='w-screen overflow-x-scroll text-red-500'>
-      {/* Wrapper */}
-      <div className='w-max flex'>
-        {/* Single Item */}
-        <div className='w-screen h-[60vh] flex flex-col items-center justify-around p-4'>
-          {/* Image container */}
-          <div className='relative flex-1 w-full'>
-            <Image src="/temporary/p1.png" alt='' fill className='object-contain' />
-          </div>
-          {/* Text Container */}
-            <div className='flex-1 flex flex-col gap-4'>
-              <h1 className='text-xl font-bold uppercase'>Title</h1>
-              <p>Desc</p>
-              <span className='text-xl font-bold'>$123</span>
-              <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+    <div className="w-screen overflow-x-scroll text-red-500">
+      {/* WRAPPER */}
+      <div className="w-max flex">
+        {/* SINGLE ITEM */}
+        {featuredProducts.map((item) => (
+          <div
+            key={item.id}
+            className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+          >
+            {/* IMAGE CONTAINER */}
+            {item.img && (
+              <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+                <Image src={item.img} alt="" fill className="object-contain" />
+              </div>
+            )}
+            {/* TEXT CONTAINER */}
+            <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
+              <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+              <p className="p-4 2xl:p-8">{item.desc}</p>
+              <span className="text-xl font-bold">${item.price}</span>
+              <button className="bg-red-500 text-white p-2 rounded-md">
+                Add to Cart
+              </button>
             </div>
-        </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
