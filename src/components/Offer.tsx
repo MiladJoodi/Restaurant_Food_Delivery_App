42eefbad3e9e86b4c38f0a7b63ed5bcdcfafdda9
Milad Joodi <winbeta.com@gmail.com>
2024-03-18 20:54:09 +0330

    update

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -4,7 +4,7 @@ import CountDown from './CountDown'
 
 function Offer() {
   return (
-    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]" >
       {/* Text Container */}
       <div className='flex-1 flex flex-col justify-center items-center text-center gap-8 p-6'>
         <h1 className='text-white text-5xl font-bold xl:text-6xl'>Delicious Burger & French Fry</h1>
